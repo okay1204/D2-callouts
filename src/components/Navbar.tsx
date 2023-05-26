a7c20e582379c01f5bed2141289b45fef0986b6a
okayyyy <zghan2005@gmail.com>
2023-05-26 11:59:11 -0700

    Make nav logo clickable

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import styles from './Navbar.module.css'
 import Image from 'next/image'
 
 export default function Navbar() {
     return (
         <nav className={styles.nav}>
-            <Image src="/logo.png" alt="D2 Callouts Logo" width={50} height={50} />
+            <Link href="/"><Image src="/logo.png" alt="D2 Callouts Logo" width={50} height={50} /></Link>
             <h1>D2 Callouts</h1>
             <h3>by okay</h3>
         </nav>
