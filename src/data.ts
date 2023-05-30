684545fad966e57e74064b1748102949efd3e65b
okayyyy <zghan2005@gmail.com>
2023-05-29 22:47:10 -0700

    Add callout data and images

---
+++ src/data.ts
@@ -0,0 +1,44 @@
+export default [
+    {
+        name: 'Wish Symbols',
+        activities: [
+            {
+                name: 'Shattered Throne',
+                imageNumbers: [1, 2, 4, 7, 9, 12, 15]
+            },
+            {
+                name: 'Last Wish',
+                imageNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+            }
+        ],
+        imageFolder: 'wish-symbols'
+    },
+    {
+        name: 'Hive Runes',
+        activities: [
+            {
+                name: 'Pit of Heresy',
+                imageNumbers: [2, 3, 4, 7, 8, 9, 10, 12, 13, 16]
+            },
+            {
+                name: 'King\'s Fall',
+                imageNumbers: [0, 1, 4, 5, 6, 7, 9, 11, 17]
+            },
+            {
+                name: 'Ghosts of the Deep',
+                imageNumbers: [0, 2, 3, 4, 5, 6, 7]
+            }
+        ],
+        imageFolder: 'hive-runes'
+    },
+    {
+        name: 'Glyphs',
+        activities: [
+            {
+                name: 'Vow of the Disciple',
+                imageNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+            }
+        ],
+        imageFolder: 'glyphs'
+    }
+]
