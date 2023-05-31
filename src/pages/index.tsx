a425fe2cc2e85322e6532c484520a70c12f78f3e
okayyyy <zghan2005@gmail.com>
2023-05-30 19:59:10 -0700

    Add home page text and image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
-export default function ActivityList() {
+import RaidEmblem from '@/images/raid-emblem.webp'
+import Image from 'next/image'
+import styles from './Home.module.css'
+
+export default function Home() {
     return (
         <main>
-            <h1>Home</h1>
+            <div className={styles.showcase}>
+                <div className={styles.showCaseText}>
+                    <h1>Get your raid team on the same page.</h1>
+                    <span>View, edit, and share callouts in an instant</span>
+                </div>
+                <div className={styles.showcaseImageContainer}>
+                    <Image className={styles.showcaseImage} src={RaidEmblem} alt='Raid Emblem' />
+                </div>
+            </div>
         </main>
     )
 }
