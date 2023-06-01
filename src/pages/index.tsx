825a2c00f7c2eefec1e75f7e20b50534315f400f
okayyyy <zghan2005@gmail.com>
2023-05-31 22:51:39 -0700

    Add mouse effect and make raid emblem brighter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
-import RaidEmblem from '@/images/raid-emblem.webp'
+import RaidEmblem from '@/images/raid-emblem.png'
 import Image from 'next/image'
 import styles from './Home.module.css'
+import { useEffect, useState } from 'react'
+
 
 export default function Home() {
+    const [mousePos, setMousePos] = useState({ x: -100, y: -100 });
+    const [visible, setVisible] = useState(false);
+
+    // Track mouse position and update showcaseMouse position
+    useEffect(() => {
+        const handleMouseMove = (event: MouseEvent) => {
+            setMousePos({ x: event.clientX, y: event.clientY });
+        };
+
+
+        window.addEventListener('mousemove', handleMouseMove);
+    
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
     return (
         <main>
-            <div className={styles.showcaseBackground}>
+            <div className={styles.showcaseBackground} onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
+                <div className={styles.showcaseMouse} style={{top: mousePos.y - 330, left: mousePos.x - 250, visibility: visible ? 'visible' : 'hidden'}}>
+                </div>
                 <div className={styles.showcaseBackgroundFade}>
                     <div className={styles.showcase}>
                         <div className={styles.showCaseText}>
