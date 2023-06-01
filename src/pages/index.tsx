94c5c2c180cfa01a2338d4c7734a6f2ec8a9a5e9
okayyyy <zghan2005@gmail.com>
2023-05-31 19:04:14 -0700

    Add glowing grid background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,17 @@ import styles from './Home.module.css'
 export default function Home() {
     return (
         <main>
-            <div className={styles.showcase}>
-                <div className={styles.showCaseText}>
-                    <h1>Get your raid team on the same page.</h1>
-                    <span>View, edit, and share callouts in an instant</span>
-                </div>
-                <div className={styles.showcaseImageContainer}>
-                    <Image className={styles.showcaseImage} src={RaidEmblem} alt='Raid Emblem' />
+            <div className={styles.showcaseBackground}>
+                <div className={styles.showcaseBackgroundFade}>
+                    <div className={styles.showcase}>
+                        <div className={styles.showCaseText}>
+                            <h1>Get your raid team on the same page.</h1>
+                            <span>View, edit, and share callouts in an instant</span>
+                        </div>
+                        <div className={styles.showcaseImageContainer}>
+                            <Image className={styles.showcaseImage} src={RaidEmblem} alt='Raid Emblem' />
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
