e63edaa182f7858d9ce169071e77463530a25318
okayyyy <zghan2005@gmail.com>
2023-06-30 15:26:00 -0700

    Update auther metadata with correct urls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     description: 'Cooperate with your team and master your raid callouts',
     viewport: 'width=device-width, initial-scale=1',
     applicationName: 'D2 Callouts',
-    authors: [{ name: 'okay1204', url: 'https://github.com/okay1204'}, { name: 'Shadowttk', url: 'https://www.twitch.tv/shadowttk'}],
+    authors: [{ name: 'okay1204', url: 'https://linktr.ee/okay1204'}, { name: 'Shadowttk', url: 'https://linktr.ee/shadowttk'}],
     generator: 'Next.js',
     keywords: ['d2', 'destiny', 'destiny 2', 'callouts', 'd2 callouts', 'destiny callouts', 'vow', 'hive', 'last wish', 'symbols', 'icons', 'glyphs', 'runes', 'raid', 'raid callouts', 'raid symbols', 'raid icons', 'raid glyphs', 'raid runes'],
     icons: '/favicon.ico',
