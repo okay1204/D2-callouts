a32f37f81a5a84a8600e84f3ef2b7b5891050a70
okayyyy <zghan2005@gmail.com>
2023-08-04 22:42:15 -0700

    Attempt at resizing textboxes

--- src/app/callout/[calloutSetId]/Symbol.tsx
+++ src/app/callout/[calloutSetId]/Symbol.tsx
@@ -12,14 +12,17 @@ interface SymbolProps {
     setInEditMode: (inEditMode: boolean) => void
     onLoadingComplete: () => void
     name: string
-    onNameChange: (ChangeEvent: ChangeEvent<HTMLInputElement>) => void
+    onNameChange: (ChangeEvent: ChangeEvent<HTMLTextAreaElement>) => void
     restoreDefaultsClicked: boolean
 }
 
-export default function Symbol({imageReference, selectorClassName, inEditMode, setInEditMode, onLoadingComplete, name, onNameChange, restoreDefaultsClicked}: SymbolProps) {
-    const inputRef = useRef<HTMLInputElement>(null);
+const maxRows = 2;
+const textareaLineHeight = 30;
+export default function Symbol({ imageReference, selectorClassName, inEditMode, setInEditMode, onLoadingComplete, name, onNameChange, restoreDefaultsClicked }: SymbolProps) {
+    const inputRef = useRef<HTMLTextAreaElement>(null);
     const [wasClicked, setWasClicked] = useState<boolean>(false);
     const [originalName, setOriginalName] = useState<string | undefined>(undefined);
+    const [rows, setRows] = useState<number>(1);
 
     // If the component is clicked, focus the input element
     useEffect(() => {
@@ -32,6 +35,30 @@ export default function Symbol({imageReference, selectorClassName, inEditMode, s
     const clearOriginalName = () => {
         originalName != undefined && setOriginalName(undefined);
     }
+
+    // Write an updaterows function that will make the textarea grow as the user types, but only to a max of 2 rows. Make sure to use the state setter function to update the rows state variable.
+    const updateRows = () => {
+        if (inputRef.current != null) {
+            const previousRows = inputRef.current.rows;
+            inputRef.current.rows = 1; // Reset the number of rows in the textarea
+            const currentRows = ~~(inputRef.current.scrollHeight / textareaLineHeight);
+
+            if (currentRows === previousRows) {
+                inputRef.current.rows = currentRows;
+            }
+
+            if (currentRows >= maxRows) {
+                inputRef.current.rows = maxRows;
+                inputRef.current.scrollTop = inputRef.current.scrollHeight;
+            }
+
+            setRows(currentRows < maxRows ? currentRows : maxRows);
+        }
+    }
+
+    useEffect(() => {
+        updateRows();
+    }, [name]);
 
     return (
         <div
@@ -55,19 +82,23 @@ export default function Symbol({imageReference, selectorClassName, inEditMode, s
                 />
             </div>
 
-            <input
-                type="text"
+            <textarea
                 className={styles.symbolName}
                 disabled={!inEditMode}
                 ref={inputRef}
                 // If originalName is defined, that means the text was temporarily cleared to allow the user to edit the name. If they click out without changing the name, we want to restore the original name.
                 value={originalName == undefined ? name : ''}
                 onBlur={() => clearOriginalName()}
+                rows={rows}
                 onChange={e => {
                     onNameChange(e);
-                    clearOriginalName()
+                    clearOriginalName();
+                    updateRows();
                 }}
-                style={{color: restoreDefaultsClicked ? 'white' : 'rgba(235, 235, 235, 0.8)'}}
+                style={{
+                    color: restoreDefaultsClicked ? 'white' : 'rgba(235, 235, 235, 0.8)',
+                    overflowY: rows === maxRows ? 'scroll' : 'hidden'
+                }}
             />
         </div>
     )
